f0f0e0f7717b5ed490d2bcfe8f69eecd60fa3c2b
kunal00000 <kunalverma2468@gmail.com>
2022-08-22 15:49:31 +0530

    lk

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+fetch("data.json").then((data)=>{
+    return data.json()
+}).then((compeleteData)=>{
+    let postData =""
+    compeleteData.map((kunal,index)=>{
+postData += ` <div class="inele">
+<div class="number">0${index +1}</div>
+<div class="float">
+    <p class="name">
+      ${kunal.name}
+    </p>
+    <p class="catchline">
+        ${kunal.catchline}
+    </p>
+    <p class="date">
+      ${kunal.date}
+    </p>
+</div>
+</div>`
+document.getElementById("trendingPosts").innerHTML = postData
+    })
+})
