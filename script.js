e823cb868478d7248d3a4e5a6236600832d1b469
kunal00000 <kunalverma2468@gmail.com>
2022-08-26 22:26:15 +0530

    new Commit

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ fetch("data.json").then((data)=>{
     compeleteData.map((kunal,index)=>{
 postData += ` <div class="inele">
 <div class="number">0${index +1}</div>
-<div class="float">
+    <div class="proficon">
+        <img src=${kunal.proficon}  height="25px">
+    </div>
     <p class="name">
       ${kunal.name}
     </p>
@@ -15,8 +17,40 @@ postData += ` <div class="inele">
     <p class="date">
       ${kunal.date}
     </p>
-</div>
 </div>`
 document.getElementById("trendingPosts").innerHTML = postData
+    })
+})
+
+fetch("dataTwo.json").then((data1)=>{
+    return data1.json()
+}).then((compeleteData1)=>{
+    let postData1 =""
+    compeleteData1.map((kunal1)=>{
+postData1 += `<div class="blog1">
+    <div class="blogRead">
+        <div class="proficon">
+            <img src=${kunal1.proficon} height="25px">
+        </div>
+        <p class="name">
+            ${kunal1.name}
+        </p>
+        <p class="catchline1">
+            ${kunal1.catchline1}
+        </p>
+        <p class="catchline2">
+            ${kunal1.catchline2}
+        </p>
+        <p class="dateBlog">
+            ${kunal1.dateBlog}
+            <button class="tagButton">${kunal1.tagButton}</button>
+            <button class="bookicon">icon</button>
+        </p>
+    </div>
+    <div class="imageBlog">
+        <img src="./guerrillabuzz-crypto-pr-PACWvLRNzj8-unsplash.jpg" alt="">
+    </div>
+</div>`
+document.getElementsById("leftBlog").innerHTML = postData1
     })
 })
