fa418843d5a89e0f6402e1070715a65dcb1d751f
kunal00000 <kunalverma2468@gmail.com>
2022-09-03 03:36:40 +0530

    all relatives set

--- script.js
+++ script.js
@@ -4,19 +4,21 @@ fetch("data.json").then((data)=>{
     let postData =""
     compeleteData.map((kunal,index)=>{
 postData += ` <div class="inele">
-<div class="number">0${index +1}</div>
-    <div class="proficon">
-        <img src=${kunal.proficon}  height="25px">
-    </div>
-    <p class="name">
-      ${kunal.name}
-    </p>
-    <p class="catchline">
-        ${kunal.catchline}
-    </p>
-    <p class="date">
-      ${kunal.date}
-    </p>
+    <div class="number">0${index +1}</div>
+    <div class="numberRight">
+        <div class="proficon">
+            <img src=${kunal.proficon}  height="25px">
+        </div>
+        <p class="name">
+        ${kunal.name}
+        </p>
+        <p class="catchline">
+            ${kunal.catchline}
+        </p>
+        <p class="date">
+        ${kunal.date}
+        </p>
+    </div>    
 </div>`
 document.getElementById("trendingPosts").innerHTML = postData
     })
@@ -42,7 +44,7 @@ postData1 += `<div class="blog1">
             ${kunal1.catchline2}
         </p>
         <p class="dateBlog">
-            ${kunal1.dateBlog}
+            <button class="daty">${kunal1.dateBlog}</button>
             <button class="tagButton">${kunal1.tagButton}</button>
             <button class="star">
                 <img src="./Screenshot_2022-08-26_at_11.54.03_PM-removebg-preview.png" height="22px">
